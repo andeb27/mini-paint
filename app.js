a562a4dc6cffe4db90dd80c17db68664d62fde38
Andrew Bowers <andeb27@gmail.com>
2017-01-12 12:31:39 -0700

    mini-paint complete

---
+++ app.js
@@ -0,0 +1,31 @@
+$('document').ready(function() {
+    var $box = $('.box');
+    var $reset = $('#reset');
+    var $color = 'white';
+    $box.on('click', function() {
+        $(this).addClass($color);
+    });
+
+    $box.on('dblclick', function() {
+        $(this).removeClass('white');
+    });
+
+    $reset.on('click', function() {
+        $box.removeClass('white red green blue yellow');
+    });
+    $('#red').on('click', function() {
+        $color = 'red';
+    });
+     $('#green').on('click', function() {
+        $color = 'green';
+    });
+     $('#blue').on('click', function() {
+        $color = 'blue';
+    });
+     $('#yellow').on('click', function() {
+        $color = 'yellow';
+    });
+    $('#white').on('click', function() {
+        $color = 'white';
+    });
+});
